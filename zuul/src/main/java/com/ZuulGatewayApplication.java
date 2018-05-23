c948edce2103d11d51618c6ca16111224bf9df87
yingyang <393050600@qq.com>
2018-05-23 15:36:06 +0800

    zuul 版本暂时停用

--- zuul/src/main/java/com/ZuulGatewayApplication.java
+++ zuul/src/main/java/com/ZuulGatewayApplication.java
@@ -2,14 +2,14 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+//import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
 /**
  * Created by yang on 2018/1/9.
  */
 @SpringCloudApplication
-@EnableZuulProxy
+//@EnableZuulProxy
 public class ZuulGatewayApplication {
     
     @Bean
