73561c19ae00f409ba9529050d0fa6e50eed53d8
yingyang <393050600@qq.com>
2018-05-23 14:54:28 +0800

    springcloud 版本更新, springboot版本更新

--- eureka-service-consumer/src/main/java/com/EurekaConsumerApplication.java
+++ eureka-service-consumer/src/main/java/com/EurekaConsumerApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
--- feign/src/main/java/com/FeignApplication.java
+++ feign/src/main/java/com/FeignApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * Created by yang on 2018/1/9.
