9125a876fa86625506f447203727af8cac16167f
yingyang <393050600@qq.com>
2018-07-30 09:21:51 +0800

    service重命名

---
+++ eureka-service-provider/src/main/java/com/DefaultController.java
@@ -0,0 +1,24 @@
+package com;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by yang on 2018/1/6.
+ */
+@RestController
+public class DefaultController extends BaseController {
+    
+    @Autowired
+    private DiscoveryClient discoveryClient;
+    
+    @RequestMapping(value = "/service", method = RequestMethod.GET)
+    public void service() {
+        
+        log.info("eureka-service: " + discoveryClient.getServices());
+    }
+    
+}
