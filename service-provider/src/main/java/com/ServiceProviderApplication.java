2bc65f63ff1cfe8133fe42731439f4f9da388be0
yingyang <393050600@qq.com>
2018-09-12 16:34:49 +0800

    重构

--- service-provider/src/main/java/com/ServiceProviderApplication.java
+++ service-provider/src/main/java/com/ServiceProviderApplication.java
@@ -1,11 +1,11 @@
 package com;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -17,7 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableFeignClients
 @EnableSwagger2
 public class ServiceProviderApplication {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(ServiceProviderApplication.class, args);
     }
